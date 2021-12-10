5d2976d62b4cbce4a3eaa2d5742ec7810bee0cb4
Cameron Grieder <cjgrieder@gmail.com>
2021-12-10 09:12:53 -0700

    orders.js completed

--- orders.js
+++ orders.js
@@ -30,7 +30,18 @@
 */
 
 //CODE HERE
-
+class Ticket {
+    constructor (items, orderTime, customerId){
+    this.items = items
+    this.orderTime = orderTime
+    this.customerId = customerId
+    this.status = `queued`
+}
+updateStatus(newStatus){
+    this.status = newStatus
+    console.log(`the order for ${this.customerId} is now ${this.status}`)
+}
+}
 
 
 /*
@@ -45,7 +56,8 @@
 */
 
 //CODE HERE
-
+let firstTicket = new Ticket(`pizza, bread, a soda`,`9:00`,17)
+console.log(firstTicket)
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +66,5 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus(`cooking`)
+console.log(firstTicket)
